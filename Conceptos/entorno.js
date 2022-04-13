e34681275707b08766b8adf3ccda42df72ac21ba
Federico Villace <federicovillace@gmail.com>
2022-04-13 13:52:04 -0300

    Node Fundaments

---
+++ Conceptos/entorno.js
@@ -0,0 +1,4 @@
+let nombre = process.env.NOMBRE || "sin nombre";
+
+
+console.log("hola", nombre);
---
+++ Conceptos/monohilo.js
@@ -0,0 +1,15 @@
+console.log("hola mundo");
+let i = 0;
+setInterval(function () {
+  try {
+    console.log(i);
+    i++;
+    if (i === 5) {
+      let a = 3 + z;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}, 1000);
+
+console.log("segunda instruccion");
