4c701a3812dd4a4a2c1798738ac0bc38ec62c246
Federico Villace <federicovillace@gmail.com>
2022-04-13 19:47:57 -0300

    Update promises.js

--- async/promises.js
+++ async/promises.js
@@ -1,20 +1,35 @@
-const hello = (name, callback) => {
-  setTimeout(() => {
-    console.log("Hi " + name);
-    callback(name);
-  }, 2000);
+const hello = (name) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Hi " + name);
+      resolve(name);
+    }, 2000);
+  });
 };
 
-const bye = (name, callback) => {
-  setTimeout(() => {
-    console.log("Bye ", name);
-    callback();
-  }, 1000);
+const bye = (name) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Bye ", name);
+      resolve();
+    }, 1500);
+  });
 };
 
-const talk = (callback) => {
-  setTimeout(() => {
-    console.log("bla bla bla bla");
-    callback();
-  }, 1000);
+const talk = (name) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("bla bla bla bla");
+      resolve(name);
+    }, 1000);
+  });
 };
+
+//--------------
+
+hello("carlos")
+  .then(talk)
+  .then(bye)
+  .then(() => {
+    console.log("terminando el proceso");
+  });
